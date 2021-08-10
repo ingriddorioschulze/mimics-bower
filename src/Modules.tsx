a6245de0263a69ed36e697f043b60b0f731f4d70
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2021-08-10 10:55:20 +0200

    refactor logic to get the data from api using urlparams

--- src/Modules.tsx
+++ src/Modules.tsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import Pagination from '@material-ui/lab/Pagination'
 
-export type ModulesObject = {
+export type ModulesArray = {
   name: string
   stars: number
   owner: string
 }
 
-const renderModules = (modules: []) => {
-  return (
-     {modules
-      .sort((a, b) => (a.stars < b.stars ? 1 : -1))
-        .map((module) => (
-          <div key={module.name}>
-            <p>{module.name}</p>
-            <p>{module.stars}</p>
-            {/* add the owner */}
-          </div>
-        ))}
-  )
-}
-
 function Modules() {
-  const [modules, setModules] = useState<ModulesObject[]>([])
+  const [modules, setModules] = useState<ModulesArray[]>([])
   const [error, setError] = useState('')
-  const [itemsPerPage, setItemsPerPage] = useState(5)
   const [page, setPage] = React.useState(1)
 
+  const itemsPerPage = 5
+
   useEffect(() => {
-    fetch('https://libraries.io/api/bower-search?q=')
+    const query = new URLSearchParams({
+      page: page.toFixed(0),
+      per_page: itemsPerPage.toFixed(0),
+      q: '',
+      sort: 'stars',
+    }).toString()
+    fetch('https://libraries.io/api/bower-search?' + query)
       .then((response) => {
         if (response.ok) return response.json()
         throw new Error('something went wrong in the modules request')
@@ -39,7 +32,7 @@ function Modules() {
       .catch((error) => {
         setError(error.message)
       })
-  }, [])
+  }, [page])
 
   if (error) return <h1>{error}</h1>
 
@@ -54,21 +47,18 @@ function Modules() {
       <div>
         <input type="search" id="search" name="search"></input>
       </div>
-      {modules
-        .slice(0, itemsPerPage ? itemsPerPage : modules.length)
-        .sort((a, b) => (a.stars < b.stars ? 1 : -1))
-        .map((module) => (
-          <div key={module.name}>
-            <p>{module.name}</p>
-            <p>{module.stars}</p>
-            {/* add the owner */}
-          </div>
-        ))}
+      {modules.map((module) => (
+        <div key={module.name}>
+          <p>{module.name}</p>
+          <p>{module.stars}</p>
+          {/* add the owner */}
+        </div>
+      ))}
       <Pagination
-        count={5}
+        count={100}
         page={page}
+        siblingCount={2}
         onChange={handleChange}
-        showFirstButton
         showLastButton
       />
     </div>
