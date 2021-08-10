1f956e849da4e95e77a3bb15d7977bdf530c7872
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2021-08-10 15:42:20 +0200

    add logi to the search

--- src/Modules.tsx
+++ src/Modules.tsx
@@ -9,8 +9,10 @@ export type ModulesArray = {
 
 function Modules() {
   const [modules, setModules] = useState<ModulesArray[]>([])
-  const [error, setError] = useState('')
+  const [error, setError] = useState(false)
   const [page, setPage] = React.useState(1)
+  const [search, setSearch] = useState('')
+  const [loading, setLoading] = useState(true)
 
   const itemsPerPage = 5
 
@@ -18,47 +20,71 @@ function Modules() {
     const query = new URLSearchParams({
       page: page.toFixed(0),
       per_page: itemsPerPage.toFixed(0),
-      q: '',
-      sort: 'stars',
+      q: search.toString(),
+      // sort: 'stars',
     }).toString()
+
     fetch('https://libraries.io/api/bower-search?' + query)
       .then((response) => {
         if (response.ok) return response.json()
         throw new Error('something went wrong in the modules request')
       })
       .then((modules) => {
+        setLoading(false)
         setModules(modules)
       })
       .catch((error) => {
         setError(error.message)
       })
-  }, [page])
+  }, [page, search])
 
   if (error) return <h1>{error}</h1>
 
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+  const handlePaginationChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
     setPage(value)
   }
 
-  console.log(modules)
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.currentTarget.value)
+  }
 
   return (
     <div>
       <div>
-        <input type="search" id="search" name="search"></input>
+        <input
+          type="search"
+          id="search"
+          name="search"
+          placeholder="Search..."
+          value={search}
+          onChange={handleSearchChange}
+        ></input>
       </div>
-      {modules.map((module) => (
-        <div key={module.name}>
-          <p>{module.name}</p>
-          <p>{module.stars}</p>
-          {/* add the owner */}
-        </div>
-      ))}
+      {loading && <div>Loading...</div>}
+      {!loading && !error && modules.length > 0 ? (
+        modules
+          .sort((a, b) => (a.stars < b.stars ? 1 : -1))
+          .map((module) => (
+            <div key={module.name}>
+              <p>{module.name}</p>
+              <p>{module.stars}</p>
+              {/* add the owner */}
+            </div>
+          ))
+      ) : (
+        <h1>No results, please try different query</h1>
+      )}
+
+      {error && <div>Error message</div>}
+
       <Pagination
         count={100}
         page={page}
         siblingCount={2}
-        onChange={handleChange}
+        onChange={handlePaginationChange}
         showLastButton
       />
     </div>
